6f3ef3f501ae5db009ed7a79481a461526865770
ryokf <ryokhrisnaf@gmail.com>
2026-03-08 00:58:39 +0700

    feat: Implement robust error handling for user registration and add `.DS_Store` to gitignore files.

--- apps/api/src/controllers/user_controller.rs
+++ apps/api/src/controllers/user_controller.rs
@@ -1,4 +1,4 @@
-use axum::{ Json, extract::{ State } };
+use axum::{ Json, extract::State, http::StatusCode };
 use crate::{ dto::user_dto::CreateUserDto, models::user::{ Model as User } };
 
 use crate::config::db::AppState;
@@ -6,6 +6,12 @@ use crate::config::db::AppState;
 pub async fn register(
     State(state): State<AppState>,
     Json(request): Json<CreateUserDto>
-) -> Json<User> {
-    crate::services::user_service::register(request, State(state)).await
+) -> Result<Json<User>, (StatusCode, String)> {
+    match crate::services::user_service::register(request, State(state)).await {
+        Ok(new_user) => Ok(Json(new_user)),
+        Err(e) => {
+            let message = format!("Failed to register : {}", e);
+            Err((StatusCode::INTERNAL_SERVER_ERROR, message))
+        }
+    }
 }
--- apps/api/src/services/user_service.rs
+++ apps/api/src/services/user_service.rs
@@ -3,17 +3,15 @@ use crate::{
     dto::user_dto::CreateUserDto,
     models::user::{ ActiveModel, Model },
 };
-use axum::{ Json, extract::{ State } };
-use sea_orm::{ ActiveModelTrait, ActiveValue::Set };
+use axum::{ extract::{ State } };
+use sea_orm::{ ActiveModelTrait, ActiveValue::Set, DbErr };
 
-pub async fn register(data: CreateUserDto, state: State<AppState>) -> Json<Model> {
+pub async fn register(data: CreateUserDto, state: State<AppState>) -> Result<Model, DbErr> {
     let new_user = ActiveModel {
         email: Set(data.email),
         password_hash: Set(data.password_hash),
         ..Default::default()
     };
 
-    let inserted_user = new_user.insert(&state.db).await.unwrap();
-
-    Json(inserted_user)
+    new_user.insert(&state.db).await
 }
