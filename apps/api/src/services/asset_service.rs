04fbfbef67a0dd1af44c4d63e764ea48e5c31583
ryokf <ryokhrisnaf@gmail.com>
2026-03-11 06:00:07 +0700

    feat: Add specific price calculation logic for the 'GC=F' ticker.

--- apps/api/src/services/asset_service.rs
+++ apps/api/src/services/asset_service.rs
@@ -7,11 +7,14 @@ pub async fn get_latest_price(ticker: &str) -> Result<f64, Box<dyn std::error::E
 
     let quote = response.last_quote()?;
 
-    // Mendapatkan 2 karakter terakhir dari ticker
-    let last_two = if ticker.len() >= 2 { &ticker[ticker.len() - 2..] } else { ticker };
+    if ticker == "GC=F" {
+        return Ok(usd_to_idr_convert(quote.close).await? / 31.1035);
+    }
 
+    let last_two = if ticker.len() >= 2 { &ticker[ticker.len() - 2..] } else { ticker };
     if last_two != "JK" {
         return Ok(usd_to_idr_convert(quote.close).await?);
     }
+
     Ok(quote.close)
 }
